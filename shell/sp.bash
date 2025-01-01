4ac5179906c2d93709bbfa15a7ba830f4facece4
Ian Park <contact@duckonomy.com>
2025-01-01 20:55:51 +0900

    update shell integration

--- shell/sp.bash
+++ shell/sp.bash
@@ -1,4 +1,7 @@
-# shellcheck shell=bash
+function __sp_pwd() {
+    \builtin pwd -L
+}
+
 function __sp_cd() {
     # shellcheck disable=SC2164
     \builtin cd -- "$@"
@@ -9,18 +12,50 @@ function __sp_compile() {
 }
 
 function __sp_cd_root() {
-    __sp_cd $(sp)
+    __sp_cd $(pk)
 }
 
 function __sp_grep_edit() {
-    RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case"
+    rm -f /tmp/rg-fzf-{r,f}
+    RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
     INITIAL_QUERY="${*:-}"
-    command fzf --ansi --disabled --query "$INITIAL_QUERY" --bind="ctrl-c:abort" --bind "start:reload:$RG_PREFIX {q} $(sp)" --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" --delimiter : --preview 'bat --color=always {1} --highlight-line {2}' --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' --bind 'enter:become($EDITOR {1} +{2})'
+    fzf --ansi --disabled --query "$INITIAL_QUERY" \
+        --bind "start:reload:$RG_PREFIX {q}" \
+        --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
+        --bind 'ctrl-t:transform:[[ ! $FZF_PROMPT =~ ripgrep ]] &&
+        echo "rebind(change)+change-prompt(1. ripgrep> )+disable-search+transform-query:echo \{q} > /tmp/rg-fzf-f; cat /tmp/rg-fzf-r" ||
+        echo "unbind(change)+change-prompt(2. fzf> )+enable-search+transform-query:echo \{q} > /tmp/rg-fzf-r; cat /tmp/rg-fzf-f"' \
+        --color "hl:-1:underline,hl+:-1:underline:reverse" \
+        --prompt '1. ripgrep> ' \
+        --delimiter : \
+        --header 'CTRL-T: Switch between ripgrep/fzf' \
+        --preview 'bat --color=always {1} --highlight-line {2}' \
+        --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
+        --bind 'enter:become(vim {1} +{2})'
+    }
+    # Grep edit
+    # function __sp_grep_edit() {
+    #     RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case"
+    #     INITIAL_QUERY="${*:-}"
+    #     __sp_cd ${selection}
+    #     command fzf --ansi --disabled --query "$INITIAL_QUERY" --bind="ctrl-c:abort" --bind "start:reload:$RG_PREFIX . $(pk $selection)" --bind "change:reload:sleep 0.1; $RG_PREFIX . || true" --delimiter : --preview 'bat --color=always {1} --highlight-line {2}' --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' --bind 'enter:become($EDITOR {1} +{2})'
+    #
+    #     # RG_STUFF="rg --column --line-number --no-heading --color=always --smart-case"
+    #     # INITIAL_QUERY="${*:-}"
+    #     # echo fzf --ansi --disabled --query "$INITIAL_QUERY" --bind="ctrl-c:abort" --bind "start:reload:$RG_STUFF {q} $(pk $1)" --bind "change:reload:sleep 0.1; $RG_STUFF || true" --delimiter : --preview 'bat --color=always {1} --highlight-line {2}' --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' --bind 'enter:become($EDITOR {1} +{2})'
+    #     # fzf --ansi --disabled --query "$INITIAL_QUERY" --bind="ctrl-c:abort" --bind "start:reload:$RG_STUFF {q} $(pk $1)" --bind "change:reload:sleep 0.1; $RG_STUFF || true" --delimiter : --preview 'bat --color=always {1} --highlight-line {2}' --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' --bind 'enter:become($EDITOR {1} +{2})'
+    # }
+
+function __sp_find_edit() {
+    FD_PREFIX="fd . -tf"
+    INITIAL_QUERY="${*:-}"
+    __sp_cd $1
+    fzf --ansi --disabled --query "$INITIAL_QUERY" --bind="ctrl-c:abort" --bind "start:reload:$FD_PREFIX $(pk $1)" --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" --delimiter : --preview 'bat --color=always {1} --highlight-line {2}' --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' --bind 'enter:become($EDITOR {1} +{2})'
 }
 
 function __sp_find_file() {
     \builtin local result
-    result="$(\command fd . $(sp) | fzf -- "$@")"
+    result="$(\command fd . $(pk) -tf | fzf -- "$@")"
 
     if [[ -f ${result} ]]; then;
         $EDITOR "${result}"
@@ -31,66 +66,48 @@ function __sp_find_file() {
     fi
 }
 
+# if not currently in a project (give option to add current directory/project or to go to a project, in the later case this is ran)
 # TODO show selected project on selection
-function __sp_find_other_project() {
-    s="$(\command sp list | fzf --bind="ctrl-c:abort" -- "$@")"
-    i=0
-    while true
-    do
-        command -v $s
-        d=$(echo "$s")
-        if (( $i == 0 )); then
-            if [[ $d == "" ]]; then
-                break
-            fi
-            selection=$d
-            menu=$(\command printf "find(edit)\nfind(show)\ngrep(edit)\ngo to project\n" | fzf --bind="ctrl-c:abort")
-            s=$menu "$@"
-        elif (( $i == 1 )); then
-            if [[ $d == "" ]]; then
-                break
-            fi
-            action=$d
-            if [[ $action == "find(edit)" ]]; then
-                s="$(fd . $(sp $selection) | fzf --bind="ctrl-c:abort" -- "$@")"
-            elif [[ $action == "find(show)" ]]; then
-                command fd . $(sp $selection)
-                break
-            elif [[ $action == "grep(edit)" ]]; then
-                # TODO: This switches to a different directory on query wtf
-                RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case"
-                INITIAL_QUERY="${*:-}"
-                __sp_cd ${selection}
-                command fzf --ansi --disabled --query "$INITIAL_QUERY" --bind="ctrl-c:abort" --bind "start:reload:$RG_PREFIX {q} $(sp $selection)" --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" --delimiter : --preview 'bat --color=always {1} --highlight-line {2}' --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' --bind 'enter:become($EDITOR {1} +{2})'
-                break
-            elif [[ $action == "go to project" ]]; then
-                __sp_cd ${selection}
-                break
-            else
-                break
-            fi
-        elif (( $i == 2 )); then
-            if [[ $d == "" ]]; then
-                break
-            fi
-            __sp_cd ${selection}
-            if [[ -f $d ]]; then;
-                $EDITOR $d
-            fi
+function __sp_find_project() {
+    s="$(\command pk list | fzf --bind="ctrl-c:abort" -- "$@")"
+    command -v $s
+    selection=$(echo "$s")
+    # set -E
 
-            if [[ -d $d ]]; then;
-                __sp_cd $d
-            fi
+    if [[ $selection == "" ]]; then
+        return
+    fi
 
-            break
-        else
-            break
-        fi
-        ((i++));
-    done
+    menu=$(\command printf "find(edit)\nfind(show)\ngrep(edit)\ngo to project\n" | fzf --bind="ctrl-c:abort")
+    command -v $menu
+    output=$(echo "$menu")
+
+    if [[ $output == "" ]]; then
+        return
+    fi
+
+    if [[ $output == "find(edit)" ]]; then
+        __sp_find_edit $selection
+        return
+    elif [[ $output == "find(show)" ]]; then
+        command fd . $(pk $selection)
+        return
+    elif [[ $output == "grep(edit)" ]]; then
+        rm -f /tmp/rg-fzf-{r,f}
+        RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
+        INITIAL_QUERY="${*:-}"
+        fzf --ansi --disabled --query "$INITIAL_QUERY" \
+            --bind "start:reload:$RG_PREFIX {q} $(pk $selection)" \
+            --bind "change:reload:sleep 0.1; $RG_PREFIX {q} $(pk $selection) || true"
+        return
+    elif [[ $output == "go to project" ]]; then
+        __sp_cd ${selection}
+        return
+    else
+        return
+    fi
 }
 
-# Shortcuts
 function spc() {
     __sp_compile "$@"
 }
@@ -109,5 +126,5 @@ function spr() {
 }
 
 function spp() {
-    __sp_find_other_project "$@"
+    __sp_find_project "$@"
 }
